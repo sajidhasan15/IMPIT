b4593e2172ff2de28f394fcb9980fcab6e174bf8
Sajid Hasan <58389587+sajidhasan15@users.noreply.github.com>
2023-01-24 18:09:28 +0600

    24 January 2023 Update

--- resources/views/pages/management.blade.php
+++ resources/views/pages/management.blade.php
@@ -1,21 +1,47 @@
 <style>
+
+    @font-face{
+                font-family: "Khula";
+                src: 
+                local("Khula"),
+                local("Khula"),
+                url("/assets/fonts/Khula.woff") format("woff");
+                /* font-weight: 700;
+                font-style: bold;
+                font-size: 33px; */
+            }
+    @font-face{
+                font-family: "Futura-PT-Bold";
+                src: 
+                local("Futura PT Bold"),
+                local("Futura-PT-Bold"),
+                url("/assets/fonts/FuturaPTBold.woff") format("woff");
+                font-weight: 700;
+                font-style: bold;
+                font-size: 33px;
+            }
     h3{
-        font-family: 'Futura PT';
+        font-family: 'Futura-PT-Bold';
         font-style: bold;
         font-weight: 700;
-        src: url("/assets/fonts/FuturaPTBold.woff");
+        font-size: 33px;
+    }
+    p{
+        font-family: 'Khula';
+        font-size: 21px;
+        font-weight: 300;
     }
 </style>
 <div class="bg-black text-white pt-5 pb-5">
     <div class="container mx-auto px-10">
-        <h2 class="display-4 font-bold text-uppercase text-center">IMPIT MANAGEMENT IS</h2>
+        <h3 class="text-uppercase text-center">IMPIT MANAGEMENT IS</h3>
     <div>
         <div class="grid grid-cols-1 gap-3 md:grid-cols-2 xl:grid-cols-3 pt-20 pb-2 lg:pt-10 lg:pb-4"
             style="cursor: auto">
             <div class="p-6  rounded-lg text-center">
                 <img src="/assets/img/icon1.png" alt="" style="display: block;  margin-left: auto; margin-right: auto;">
   
-                <h3 class="text-lg font-bold mb-2 pt-2">EFFECTIVE</h3>
+                <h3 class="mb-2 pt-2">EFFECTIVE</h3>
   
                 <p class="text-sm leading-6 text-white pt-4">
                     Metus potenti velit sollicitudin porttitor magnis elit
@@ -28,7 +54,7 @@
             <div class="p-6  rounded-lg text-center">
                 <img src="/assets/img/icon2.png" alt="" style="display: block;  margin-left: auto; margin-right: auto;">
   
-                <h3 class="text-lg font-bold mb-2 pt-2">EFFICIENT</h3>
+                <h3 class="mb-2 pt-2">EFFICIENT</h3>
   
                 <p class="text-sm leading-6 text-white pt-4">
                     Metus potenti velit sollicitudin porttitor magnis elit
@@ -41,7 +67,7 @@
             <div class="p-6  rounded-lg text-center" style="cursor: auto">
                 <img src="/assets/img/icon3.png" alt="" style="display: block;  margin-left: auto; margin-right: auto;">
   
-                <h3 class="text-lg font-bold mb-2 pt-2">DYNAMIC</h3>
+                <h3 class="mb-2 pt-2">DYNAMIC</h3>
   
                 <p class="text-sm leading-6 text-white pt-4">
                     Metus potenti velit sollicitudin porttitor magnis elit
